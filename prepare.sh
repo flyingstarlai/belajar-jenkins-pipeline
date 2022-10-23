93c0e652bd3b8ba356df1c2f9515009f21336d12
starlai <fstardev@gmail.com>
2022-10-23 12:29:57 +0800

    rahasia in sh

--- prepare.sh
+++ prepare.sh
@@ -2,3 +2,5 @@
 echo "Selamat Datang di Panduan Lengkap Belajar Bash Scripting Linux"
 echo "Nama aplikasi: $APPNAME"
 echo "Identifier: $APPID"
+echo "User: $CLOUD_USR"
+echo "Password: $CLOUD_PSW"
